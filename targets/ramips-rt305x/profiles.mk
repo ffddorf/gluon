c1f22ce091299bc57d6b25e17abbfaf41740d707
alexander eickhoff <alex@artbanause.de>
2015-03-25 19:54:03 +0100

    test to compile  DIR615H1

--- targets/ramips-rt305x/profiles.mk
+++ targets/ramips-rt305x/profiles.mk
@@ -4,3 +4,6 @@
 $(eval $(call GluonProfile,VOCORE))
 $(eval $(call GluonProfileFactorySuffix,VOCORE))
 $(eval $(call GluonModel,VOCORE,vocore,vocore))
+
+$(eval $(call GluonProfile,DIR615H1))
+$(eval $(call GluonModel,DIR615C1,dir-615-h1,d-link-dir-615-rev-h1))
