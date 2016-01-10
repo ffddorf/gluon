6bd7c0e8f15b9740630f062670a1adabb1c1eaf5
Heiko Wundram <heiko.wundram@freifunk-celle.de>
2016-01-10 23:30:09 +0100

    Properly name image like other D-Link images.

--- targets/ramips-rt305x/profiles.mk
+++ targets/ramips-rt305x/profiles.mk
@@ -2,4 +2,4 @@
 
 # D-Link DIR-615 D
 $(eval $(call GluonProfile,DIR-615-D))
-$(eval $(call GluonModel,DIR-615-D,dir-615-d,dlink-dir-615-d))
+$(eval $(call GluonModel,DIR-615-D,dir-615-d,d-link-dir-615-rev-d))
